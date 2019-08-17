1f8f8b739e3b2c3584f0baef3bbd0be913bea893
Kryklyvets Oleksandr <sashakryklyvets@gmail.com>
2019-08-17 17:45:33 +0300

    Tests were created

---
+++ EpamHomeWorks/UnitTestHomeWork_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestHomeWork_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestHomeWork_1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5af2d6ca-c96e-462d-8ce1-ad7624b1f7b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EpamHomeWorks/UnitTestHomeWork_1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HomeWork_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestHomeWork_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRectanglePerimetr()
+        {            
+            Coordinate leftUpCoordinate = new Coordinate(0, 3);
+            Coordinate rightDownCoordinate = new Coordinate(3, 0);
+            Rectangle rectangle = new Rectangle(leftUpCoordinate, rightDownCoordinate);
+            var expected = 12;
+            var actual = rectangle.Perimetr();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestRectangleSquare()
+        {
+            Coordinate leftUpCoordinate = new Coordinate(0, 3);
+            Coordinate rightDownCoordinate = new Coordinate(3, 0);
+            Rectangle rectangle = new Rectangle(leftUpCoordinate, rightDownCoordinate);
+            var expected = 9;
+            var actual = rectangle.Square();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
