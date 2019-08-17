5d4bf928bfc8ca6772666a3e3584fda37a0b57f4
Kryklyvets Oleksandr <sashakryklyvets@gmail.com>
2019-08-18 01:48:14 +0300

    Some changes

--- EpamHomeWorks/UnitTestHomeWork_1/UnitTest1.cs
+++ EpamHomeWorks/UnitTestHomeWork_1/UnitTest1.cs
@@ -30,5 +30,78 @@ namespace UnitTestHomeWork_1
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void TestSircleSquare()
+        {
+            Circle circle = new Circle();
+            var expected = 153.93804002591;
+            var actual = circle.Square(7);
+
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSircleLength()
+        {
+            Circle circle = new Circle();
+            var expected = 43.98229715026;
+            var actual = circle.Length(7);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSquareRoot()
+        {
+            var expected = 5;
+            var actual = MathHelper.SquareRoot(25);
+
+            Assert.AreEqual(expected, actual);
+
+            expected = 7;
+            actual = MathHelper.SquareRoot(49);
+
+            Assert.AreEqual(expected, actual);
+
+            expected = 777;
+            actual = MathHelper.SquareRoot(603729);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestPropertyBasedRectangle()
+        {            
+            Coordinate leftUpCoordinate2 = new Coordinate(0, 7);
+            Coordinate rightDownCoordinate2 = new Coordinate(7, 0);
+
+            PropertyBasedRectangle rectangle2 = new PropertyBasedRectangle(leftUpCoordinate2, rightDownCoordinate2);
+
+            var expected = 28;
+            var actual = rectangle2.Perimetr;
+
+            Assert.AreEqual(expected, actual);
+
+            expected = 49;
+            actual = rectangle2.Square;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestComplexNumber()
+        {
+            ComplexNumber firstComplex = new ComplexNumber(5, 7);
+            ComplexNumber secondComplex = new ComplexNumber(6, 8);
+
+            ComplexNumber multiplyingResult = firstComplex * secondComplex;
+
+            var expected = new ComplexNumber(30, 56); ;
+            var actual = multiplyingResult;            
+
+            Assert.ReferenceEquals(expected, actual);
+        }
     }
 }
