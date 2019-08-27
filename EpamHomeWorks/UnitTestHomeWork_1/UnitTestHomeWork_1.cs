e9606ade3dbd5f35687db02ea664c30f44564c5f
Kryklyvets Oleksandr <sashakryklyvets@gmail.com>
2019-08-27 14:49:51 +0300

    Tests were updated

--- EpamHomeWorks/UnitTestHomeWork_1/UnitTestHomeWork_1.cs
+++ EpamHomeWorks/UnitTestHomeWork_1/UnitTestHomeWork_1.cs
@@ -91,7 +91,47 @@ namespace UnitTestHomeWork_1
         }
 
         [TestMethod]
-        public void TestComplexNumber()
+        public void TestStaticRectanglePerimetr()
+        {
+            Coordinate leftUpCoordinate = new Coordinate(0, 5);
+            Coordinate rightDownCoordinate = new Coordinate(5, 0);
+            double expected = 20;
+            double actual = StaticRectangle.Perimetr(leftUpCoordinate, rightDownCoordinate);
+
+            Assert.AreEqual(expected, actual);           
+        }
+
+        [TestMethod]
+        public void TestStaticRectangleSquare()
+        {
+            Coordinate leftUpCoordinate = new Coordinate(0, 5);
+            Coordinate rightDownCoordinate = new Coordinate(5, 0);
+            double expected = 25;
+            double actual = StaticRectangle.Square(leftUpCoordinate, rightDownCoordinate);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestStaticSircleSquare()
+        {
+            double expected = 153.93804002591;
+            double actual = StaticCircle.Square(7);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestStaticSircleLength()
+        {
+            double expected = 43.98229715026;
+            double actual = StaticCircle.Length(7);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMultiplyingComplexNumber()
         {
             ComplexNumber firstComplex = new ComplexNumber(5, 7);
             ComplexNumber secondComplex = new ComplexNumber(6, 8);
@@ -100,6 +140,32 @@ namespace UnitTestHomeWork_1
 
             ComplexNumber expected = new ComplexNumber(30, 56); ;
             ComplexNumber actual = multiplyingResult;            
+
+            Assert.ReferenceEquals(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDividingComplexNumber()
+        {
+            ComplexNumber firstComplex = new ComplexNumber(5, 7);
+            ComplexNumber secondComplex = new ComplexNumber(6, 8);
+
+            ComplexNumber dividingResult = firstComplex / secondComplex;
+
+            ComplexNumber expected = new ComplexNumber(0.833333333333333, 0.875); ;
+            ComplexNumber actual = dividingResult;
+
+            Assert.ReferenceEquals(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestToStringComplexNumber()
+        {
+            ComplexNumber complexNumber = new ComplexNumber(5, 7);
+
+
+            string expected = "Real = 5   imaginary = 7";
+            string actual = complexNumber.ToString();
 
             Assert.ReferenceEquals(expected, actual);
         }
